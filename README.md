693e3beca2cc9a564aa196df711eee0a26a2e343
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2022-08-12 10:17:09 +0800

    issue提交

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# antdv-issue
-提交issue专用
+# aaa
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content">
+    <TimeRangePicker format="HH"/>
+  </div>
+</template>
+
+<script>
+import { TimeRangePicker } from 'ant-design-vue'
+export default {
+  name: 'App',
+  components: {
+    TimeRangePicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.content {
+  width: 50%;
+}
+</style>
